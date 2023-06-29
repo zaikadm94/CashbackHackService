0c32ba389a8d65ab1360b587eebfd0004ce934c8
zaikadm94 <zaikadm94@mail.ru>
2023-06-29 12:19:40 +0300

    junit4 branch add

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,39 @@
+package ru.netology.service;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class CashbackHackServiceTest {
+
+
+    @Test
+
+    public void boundary200Test() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 200;
+
+        int actual = service.remain(amount);
+        int expected = 800;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void boundary1000Test() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void boundaryZeroTest() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+        Assert.assertEquals(expected, actual);
+    }
+}
